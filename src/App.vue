756a5ea4f104382ef29f91b586eddfb844c7ca7d
Zheng Hao <zhengh513@outlook.com>
2024-02-23 16:02:24 +0800

    chore: 组件按需自动导入

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,4 @@
   </ion-app>
 </template>
 
-<script setup lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue'
-</script>
+<script setup lang="ts"></script>
--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -15,6 +15,4 @@
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue'
-</script>
+<script setup lang="ts"></script>
