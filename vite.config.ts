b58599bf29d47687800b73fedcf6bf37a17a0758
Zheng Hao <zhengh513@outlook.com>
2024-02-23 15:33:10 +0800

    chore: 安装 unplugin-auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import Components from 'unplugin-vue-components/vite'
 import { IonicResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,35 @@ export default defineConfig({
     legacy(),
     Components({
       resolvers: [IonicResolver()],
+    }),
+    AutoImport({
+      include: [/\.[tj]sx?$/, /\.vue$/, /\.vue\?vue/, /\.md$/],
+      imports: [
+        'vue',
+        'vue-router',
+        'pinia',
+        {
+          axios: [['default', 'axios']],
+        },
+        {
+          from: 'vue-router',
+          imports: ['RouteLocationRaw'],
+          type: true,
+        },
+      ],
+      ignore: ['useMouse', 'useFetch'],
+      defaultExportByFilename: false,
+      dirs: [],
+      dts: './auto-imports.d.ts',
+      ignoreDts: ['ignoredFunction', /^ignore_/],
+      vueTemplate: false,
+      resolvers: [],
+      injectAtEnd: true,
+      eslintrc: {
+        enabled: false,
+        filepath: './.eslintrc-auto-import.json',
+        globalsPropValue: true,
+      },
     }),
   ],
   resolve: {
