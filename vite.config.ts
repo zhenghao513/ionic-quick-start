7c6139c7a141612181aae54dffebce0c9f1e0ffc
Zheng Hao <zhengh513@outlook.com>
2024-02-23 15:23:54 +0800

    chore: 安装 unplugin-vue-components

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,17 @@ import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
+import Components from 'unplugin-vue-components/vite'
+import { IonicResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    legacy()
+    legacy(),
+    Components({
+      resolvers: [IonicResolver()],
+    }),
   ],
   resolve: {
     alias: {
