f5afb0432b53f26dc39e8a0254de12a2cbd8e9f4
Zheng Hao <zhengh513@outlook.com>
2024-02-23 15:12:47 +0800

    chore: 安装 tailwind

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,8 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+import './style.css'
+
 const app = createApp(App)
   .use(IonicVue)
   .use(router);
