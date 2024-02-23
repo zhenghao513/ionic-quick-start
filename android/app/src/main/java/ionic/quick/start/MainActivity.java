f763a79d91882030f4b6485a6550d59d340f513f
Zheng Hao <zhengh513@outlook.com>
2024-02-23 15:04:57 +0800

    init

---
+++ android/app/src/main/java/ionic/quick/start/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.quick.start;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.quick.start',
+  appName: 'ionic-quick-start',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
